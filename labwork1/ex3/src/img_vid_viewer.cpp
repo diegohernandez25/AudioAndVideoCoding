c9015e3c40b4e17f1fbb4829536be63ef1f709bf
Margarida Silva <margaridaocs@ua.pt>
2019-10-04 16:01:15 +0100

    Ex3: back and forth through the images

--- labwork1/ex3/src/img_vid_viewer.cpp
+++ labwork1/ex3/src/img_vid_viewer.cpp
@@ -25,6 +25,7 @@ int main(int argc, char* argv[]) {
 		std::cout << "	-v <path_to_image1> <path_to_image2> ..." << std::endl;
 		std::cout << "	or" << std::endl;
 		std::cout << "	-i <path_to_video1> <path_to_video2> ..." << std::endl;
+		std::cout << "\nPress ESC or \"q\" to quit. Press <left> and <right> arrows to move through different images" << std::endl;
 		return 0;
 	}
 
@@ -32,8 +33,11 @@ int main(int argc, char* argv[]) {
 		// TODO
 	} else {
 		cv::namedWindow("Image viewer", cv::WINDOW_AUTOSIZE);
+		int pressedKey;
+
 		for (int i = 2; i < argc; i++) {
 			image = cv::imread(argv[i], cv::IMREAD_UNCHANGED );
+			pressedKey = 0;
 
 			if(image.empty() )	{
 				std::cout << "Image file could not be open." << std::endl;
@@ -41,9 +45,21 @@ int main(int argc, char* argv[]) {
 			}
 
 			cv::imshow("Image viewer", image );
-			cv::waitKey(0);
+
+			while (true) {
+				pressedKey = cv::waitKey(0);
+				std::cout << pressedKey << std::endl;
+				if (pressedKey == 81 && i > 2) {
+					i -= 2;
+					break;
+				} else if (pressedKey == 83 && i< argc-1) {
+					break;
+				} else if (pressedKey == 113 || pressedKey == 27) {
+					return 0;
+				}
+			}
 		}
-		cv::destroyWindow( "Image viewer" );
+		cv::destroyWindow("Image viewer" );
 	}
 
 	return 0;
