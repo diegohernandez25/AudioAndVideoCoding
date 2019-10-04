e591f9e31da9588fdec84575c2c8d2946e2e872a
Margarida Silva <margaridaocs@ua.pt>
2019-10-04 18:09:03 +0100

    Ex3: add documentation

--- labwork1/ex3/src/img_vid_viewer.cpp
+++ labwork1/ex3/src/img_vid_viewer.cpp
@@ -3,18 +3,18 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 
-
+// Waits for a key input and handles it
 int wait(int &i, int len) {
 	char pressedKey = cv::waitKey(25);
-	// If left key is pressed and it's not the first file
+	// If left key is pressed and it's not the first file, go to the previous one
 	if (pressedKey == 81 && i > 2) {
 		i--;
 		return -1;
-	// If right key is pressed and it's not the last file
+	// If right key is pressed and it's not the last file, go to the next one
 	} else if (pressedKey == 83 && i<len) {
 		i++;
 		return -1;
-	// If "ESC" or "q" is pressed
+	// If "ESC" or "q" is pressed, quit
 	} else if (pressedKey == 113 || pressedKey == 27) {
 		std::cout << 3 << std::endl;
 		return 0;
@@ -25,18 +25,22 @@ int wait(int &i, int len) {
 int main(int argc, char* argv[]) {
 	bool isVid;
 
-	// read from args
+	// Read paramets from arguments
 	if (argc > 2) {
+		// Video input
 		if (argv[1] == std::string("-v")) {
 			isVid = 1;
+		// Image input
 		} else if (argv[1] == std::string("-i")) {
 			isVid = 0;
+		// Quit if given unknown argument
 		} else {
 			std::cout << "Invalid argument \"" << argv[1] << "\". Valid flags are: " << std::endl
 					  << "	-v: video files" << std::endl
 					  << "	-i: image files" << std::endl;
 			return -1;
 		}
+	// Print usage instructions if not enough parameters are given
 	} else {
 		std::cout << "Usage:" << std::endl
 				  << "	-v <path_to_image1> <path_to_image2> ..." << std::endl
@@ -46,25 +50,34 @@ int main(int argc, char* argv[]) {
 		return 0;
 	}
 
+	// Variable intialization
 	cv::Mat image;
 	cv::Mat frame;
 	cv::VideoCapture cap;
 	int i=2;
 	int ret;
 
+	// Keeps showing videos/image til a stop request is sent
 	while (true) {
-		std::cout << "Showing " << argv[i] << std::endl;
+		std::cout << "Displaying " << argv[i] << std::endl;
 
+		// Initialize image
 		if (!isVid) {
+			// Read image
 			image = cv::imread(argv[i], cv::IMREAD_UNCHANGED );
+			// Quit if not able to read image
 			if(image.empty() )	{
 				std::cout << "Image file could not be open." << std::endl;
 				return -1;
 			}
+			// Show image in a window
 			cv::imshow("Image viewer", image );
+		// Initialize video
 		} else {
+			// Open capture
 			cap.open(argv[i]);
 
+			// Quit if unable to open capture
 			if(!cap.isOpened()){
 				std::cout << "Error opening video stream or file" << std::endl;
 				return -1;
@@ -72,14 +85,19 @@ int main(int argc, char* argv[]) {
 		}
 
 		while (true) {
+			// If playing video, cycle through the frames as well
 			if (isVid) {
+				// Get next frame
 				cap >> frame;
 
 				if (frame.empty())
-				break;
+					break;
 
+				// Show the frame in a window
 				cv::imshow( "Video player", frame );
 			}
+
+			// Check for input to quit or to show next or previous file
 			ret = wait(i, argc-1);
 			// Request to quit was received
 			if (ret == 0) {
@@ -91,11 +109,12 @@ int main(int argc, char* argv[]) {
 		}
 	}
 
+	// Closes the capture
 	if (isVid) {	
 		cap.release();
 	}
 		
-	// Closes all the frames
+	// Closes all the windows
 	cv::destroyAllWindows();
 
 	return 0;
