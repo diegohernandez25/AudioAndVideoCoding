8c836f8c65c1291d73b93281a92bb1795d0d598b
Margarida Silva <margaridaocs@ua.pt>
2019-10-18 13:19:07 +0100

    Ex9: calculate maximum per pixel error

--- labwork1/ex9/src/img_vid_snr.cpp
+++ labwork1/ex9/src/img_vid_snr.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <numeric>
+#include <math.h>
 
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
@@ -11,16 +12,22 @@ int pow2(uchar elem) {
 	return int(elem) * int (elem);
 }
 
-// Calculates the PSNR for a given pair of images
-double calculatePsnr(cv::Mat mat1, cv::Mat mat2) {
-	// Initialize matrix
-	cv::Mat diff_mat;
-	std::vector<int> unrolled_data(size);
+int sumChar(uchar elem1, uchar elem2) {
+	return int(elem1) + int(elem2);
+}
 
+int sumCharInt(uchar elem1, int elem2) {
+	return int(elem1) + elem2;
+}
+
+// Calculates the PSNR for a given pair of images
+double calculatePsnr(cv::Mat mat1, cv::Mat mat2, cv::Mat& diff_mat) {
 	int size = mat1.rows*mat2.cols;
+	std::vector<int> unrolled_data(size);
 
 	// Calculate the modulus of the difference between both matrices
 	cv::absdiff(mat1, mat2, diff_mat);
+	
 	// Calculate the power of two for each element of the matrix
 	std::transform(diff_mat.begin<int>(), diff_mat.end<int>(), unrolled_data.begin(), pow2);
 	// Sum all the elements
@@ -36,8 +43,29 @@ double calculatePsnr(cv::Mat mat1, cv::Mat mat2) {
 	return 10*std::log10((255*255)/(double(sum)/size));
 }
 
+// Calculate the maximum per pixel error
+int maximumPerPixelError(cv::Mat* mats) {
+	std::vector<int> sumDiff(mats[0].cols*mats[0].rows);
+	std::vector<uchar> channels[3];
+
+	// Convert cv matrices to vectors
+	for (int i = 0; i < 3; i++) {
+		channels[i].assign(mats[i].data, mats[i].data + mats[i].total());
+	}
+	
+	// Calculate the distance of each pixel to the original one
+	for (int i = 0; i < mats[0].cols*mats[0].rows; i++) {
+		sumDiff[i] = std::sqrt(std::pow(int(channels[0][i]), 2) + std::pow(int(channels[1][i]), 2) + std::pow(int(channels[2][i]), 2));
+	}
+
+	// Return the highest value
+	return *max_element(sumDiff.begin(), sumDiff.end());
+}
+
 // Process the given matrices
 int processMats(cv::Mat orig, cv::Mat compr) {
+	cv::Mat diff_mat[3];
+
 	// Quit if not able to read images
 	if (orig.empty() || compr.empty()) {
 		std::cout << "Error: Image file could not be open." << std::endl;
@@ -55,17 +83,23 @@ int processMats(cv::Mat orig, cv::Mat compr) {
 	cv::split(orig, split_orig);
 	cv::split(compr, split_compr);
 	double psnr = 0, tmp;
+	int maxErr;
 
 	// Calculate PSNR for each channel
 	for (int i=0; i<3; i++) {
-		tmp = calculatePsnr(split_orig[i], split_compr[i]);
+		tmp = calculatePsnr(split_orig[i], split_compr[i], diff_mat[i]);
 		if (tmp < 0)
 			return -1;
 		psnr += tmp;
 	}
-	
+
+	maxErr = maximumPerPixelError(diff_mat);
+
 	// Return the average of the three channels
 	std::cout << "PSNR: " << psnr/3 << "dB" << std::endl;
+
+	
+	std::cout << "Maximum per pixel error: " << maxErr << std::endl;
 	return 0;
 }
 
@@ -102,7 +136,6 @@ int main(int argc, char* argv[]) {
 		cv::Mat image_original, image_compressed;
 		image_original = cv::imread(argv[2], cv::IMREAD_UNCHANGED );
 		image_compressed = cv::imread(argv[3], cv::IMREAD_UNCHANGED );
-
 		// Process both images
 		return processMats(image_original, image_compressed);
 	
@@ -133,7 +166,7 @@ int main(int argc, char* argv[]) {
 			
 			// Process current frame from both sources
 			processMats(frame_orig, frame_compr);
-
+			// TODO the maximum per pixel absolute error
 		}
 
 		// Closes the capture
