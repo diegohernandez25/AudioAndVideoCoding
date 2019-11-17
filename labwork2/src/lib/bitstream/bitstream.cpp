57744716a0954552ea8babf56f5304980b64b6df
Margarida Silva <margaridaocs@ua.pt>
2019-11-17 17:03:32 +0000

    Read bytes to reference

--- labwork2/src/lib/bitstream/bitstream.cpp
+++ labwork2/src/lib/bitstream/bitstream.cpp
@@ -1,56 +1,72 @@
 #include "bitstream.h"
 using namespace std;
 
-int bitstream::writeBit(uint8_t val) {
+void bitstream::writeBit(uint8_t val) {
 	byte |= (val & 0x01) << pointer;
 	if (pointer > 0) {
 		pointer--;                    
-		return 0;                                   
+		return;       
 	}                    
 	write((char*)&byte, 1);
 	pointer = 7;
 	byte = 0;
-	return 0;
 }
 
-uint8_t bitstream::writeNBits(uint32_t* val, int n){
-  uint bit;
-  for (uint i = 0; i < n; i++) {
-        bit = ((*(val + i/32)) >> i%32) & 0x1;
-    	writeBit(bit);
-	}
-	return 0;
-}
-
-uint8_t bitstream::writeNBits(uint32_t val, int n){
-  uint bit;
-  for (uint i = 0; i < n; i++) {
+void bitstream::writeNBits(uint32_t val, uint n) {
+	uint bit;
+	for (uint i = 0; i < n; i++) {
         bit = (val >> i%32) & 0x1;
     	writeBit(bit);
 	}
-	return 0;
+}
+
+void bitstream::writeNBits(uint32_t* val, uint n) {
+	uint bit;
+	for (uint i = 0; i < n; i++) {
+        bit = ((*(val + i/32)) >> i%32) & 0x1;
+    	writeBit(bit);
+	}
 }
 
 uint8_t bitstream::readBit() {
 	uint8_t bit;
-	if (pointer == 7) {
+	if (pointer < 0) {
 		read((char*)&byte, 1); 
+		pointer = 7;
 	}
 	bit = ((byte >> pointer) & 0x01);
 	pointer--;
-	if (pointer < 0)
-		pointer = 7;
 	return bit;
 }
 
-uint32_t bitstream::readNBits(uint n){
+void bitstream::readBit(uint8_t* bit) {
+	if (pointer < 0) {
+		read((char*)&byte, 1); 
+		pointer = 7;
+	}
+	*bit = ((byte >> pointer) & 0x01);
+	pointer--;
+}
+
+// TODO assert n <= 33
+uint32_t bitstream::readNBits(uint n) {
 	uint32_t value = 0;
-	for (int i = 0; i < n; i++){
+	for (int i = 0; i < n; i++) {
 		value = value << 1 | readBit();
 	}
 	return value;
 }
 
+void bitstream::readNBits(uint32_t* bits, uint n) {
+	uint32_t value = 0;
+	uint32_t ref;
+	for (uint i = 0; i < n; i++) {
+		ref = (*(bits + i/32));
+		ref = ref | readBit();
+	}
+}
+
 bitstream::~bitstream() {
+	// TODO padding
 	close();
 }
