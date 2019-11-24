761c7eb46ec473343856a5e4255d6f8a64a05cc1
André Pinho <andre.s.pinho@ua.pt>
2019-11-24 20:10:10 +0000

    Y4M module fully functional (only unneeded details are left behind

--- labwork3/src/lib/y4m/y4m.cpp
+++ labwork3/src/lib/y4m/y4m.cpp
@@ -1,8 +1,16 @@
 #include "y4m.h"
 
+//Documentation about the Y4M (YUV4MPEG) format
+//can be found on:
+//https://wiki.multimedia.cx/index.php?title=YUV4MPEG2
+//or on the Linux Man Page (5) of yuv4mpeg:
+//$man 5 yuv4mpeg
+
 void y4m::print_header(){
 	std::cout<<"Width: "<<width<<" Height: "<<height<<std::endl;
 	std::cout<<"Framerate: "<<fps[0]<<":"<<fps[1]<<std::endl;
+	std::cout<<"Num of frame: "<<v_y.size()<<std::endl;
+	std::cout<<"Duration: "<<v_y.size()*fps[1]/(float)fps[0]<<"s"<<std::endl;
 	std::cout<<"Interlacing: ";
 	switch(interlacing){
 		case IL_PROGRESSIVE:
@@ -45,7 +53,13 @@ void y4m::print_header(){
 }
 bool y4m::load(std::string &y4mfile){
 	inf.open(y4mfile,std::fstream::in | std::fstream::binary);
-	
+	if(!load_header()) return false;
+	while(fetch()); //Get all frames
+	inf.close();
+	return true;
+}	
+bool y4m::load_header(){
+
 	std::string header;
 	std::getline(inf,header);
 
@@ -103,6 +117,9 @@ bool y4m::load(std::string &y4mfile){
 					case 'm':
 						interlacing=IL_MIXED;
 						break;
+					case '?':
+						interlacing=IL_UNSET;
+						break;
 				}
 				break;
 			case 'C': //Colour space
@@ -127,36 +144,105 @@ bool y4m::load(std::string &y4mfile){
 		if(nextptr==std::string::npos) break;
 		currptr=nextptr+1;
 	}
-	//Recreate Mats
-	y.create(height,width,CV_8U);
-	switch(color_space){
-		case CS444:
-			u.create(height,width,CV_8U);
-			v.create(height,width,CV_8U);
-			break;
-		case CS422:
-			u.create(height,(width+1)/2,CV_8U);
-			v.create(height,(width+1)/2,CV_8U);
-			break;
-		case CS420:
-		case CS420JPEG:
-		case CS420PALDV:
-			u.create((height+1)/2,(width+1)/2,CV_8U);
-			v.create((height+1)/2,(width+1)/2,CV_8U);
-			break;
-	}
+
+	v_y.clear();
+	v_u.clear();
+	v_v.clear();
+	frame_ptr=0;
+	build_structure();
 
 	return true;
 }
 
-bool y4m::next_frame(){
+void y4m::save(std::string &y4mfile){
+	outf.open(y4mfile,std::fstream::out | std::fstream::binary | std::fstream::trunc);
+	save_header();
+	store_all();
+	outf.close();
+}
+
+void y4m::save_header(){
+	outf<<magic<<"W"<<width<<" H"<<height
+		<<" F"<<fps[0]<<":"<<fps[1]
+		<<" A"<<pix_asp[0]<<":"<<pix_asp[1]
+		<<" I";
+
+	if(interlacing==IL_PROGRESSIVE)
+		outf<<"p";
+	else if(interlacing==IL_TOP_FIELD_FIRST)
+		outf<<"t";
+	else if(interlacing==IL_BOT_FIELD_FIRST)
+		outf<<"b";
+	else if(interlacing==IL_MIXED)
+		outf<<"m";
+	else /*if(interlacing==IL_UNSET)*/
+		outf<<"?";
+
+	outf<<" C";
+	if(color_space==CS444)	
+		outf<<"444";
+	else if(color_space==CS422)	
+		outf<<"422";
+	else if(color_space==CS420)	
+		outf<<"420";
+	else if(color_space==CS420JPEG)	
+		outf<<"420jpeg";
+	else /*if(color_space==CS420PALDV)*/
+		outf<<"420paldv";
+
+	outf<<std::endl;
+}
+
+void y4m::store_all(){
+	for(uint i=0;i<v_y.size();i++){
+		//Magic
+		outf<<frame_start;
+		//TODO frame specific values	
+		outf<<std::endl;
+
+		//Store y
+		outf.write((char*) v_y[i].ptr(),s_y.height*s_y.width);
+
+		//Store u
+		outf.write((char*) v_u[i].ptr(),s_u.height*s_u.width);
+
+		//Store v
+		outf.write((char*) v_v[i].ptr(),s_v.height*s_v.width);
+	}
+}
+
+void y4m::build_structure(){
+	s_y=cv::Size(width,height);	
+	switch(color_space){
+		case CS444:
+			s_u=cv::Size(width,height);
+			s_v=cv::Size(width,height);
+			break;
+		case CS422:
+			s_u=cv::Size((width+1)/2,height);
+			s_v=cv::Size((width+1)/2,height);
+			break;
+		case CS420:
+		case CS420JPEG:
+		case CS420PALDV:
+			s_u=cv::Size((width+1)/2,(height+1)/2);
+			s_v=cv::Size((width+1)/2,(height+1)/2);
+			break;
+	}
+}
+
+
+bool y4m::fetch(){
+	cv::Mat y(s_y,CV_8U);
+	cv::Mat u(s_u,CV_8U);
+	cv::Mat v(s_v,CV_8U);
+
 	std::string frame_header;
 	std::getline(inf,frame_header);
 
 	if(frame_start.compare(frame_header.substr(0,frame_start.size()))!=0){
 		return false;	
 	}
-
 
 	size_t currptr=frame_header.find(" ");
 	size_t nextptr;
@@ -185,36 +271,117 @@ bool y4m::next_frame(){
 	//Read v
 	inf.read((char*) v.ptr(),v.rows*v.cols);	
 
+	v_y.push_back(y);
+	v_u.push_back(u);
+	v_v.push_back(v);
+
 	return true;
 }
+
+void y4m::init(uint width,uint height,uint8_t color_space){
+	this->width=width;
+	this->height=height;
+	this->color_space=color_space;
+    fps[0]=0; fps[1]=0;
+    interlacing=IL_UNSET;
+    pix_asp[0]=0; pix_asp[1]=0;
+
+	v_y.clear();	
+	v_u.clear();	
+	v_v.clear();	
+	frame_ptr=0;
+	build_structure();
+}
+
+void y4m::set_framerate(uint d,uint n){
+	fps[0]=d;
+	fps[1]=n;
+}
+
+void y4m::set_aspect(uint d,uint n){
+	pix_asp[0]=d;
+	pix_asp[1]=n;
+}
+
+void y4m::set_interlace(uint8_t intr){ interlacing=intr; }
+
+bool y4m::push_frame(cv::Mat &y,cv::Mat &u,cv::Mat &v){
+	if(y.rows!=s_y.height||y.cols!=s_y.width||	
+	   u.rows!=s_u.height||u.cols!=s_u.width||	
+	   v.rows!=s_v.height||v.cols!=s_v.width)
+		return false;	
+
+	v_y.push_back(y);
+	v_u.push_back(u);
+	v_v.push_back(v);
+	return true;
+}
+
+bool y4m::seek(uint frame_number){
+	frame_ptr=frame_number;
+	return frame_ptr<v_y.size();
+}
+bool y4m::next_frame(){
+	frame_ptr++;
+	return frame_ptr<v_y.size();
+}
+
 
 cv::Mat y4m::get_bgr(){
 	cv::Mat adj_u,adj_v;
 	cv::Mat yuv,bgr;
-	cv::resize(u,adj_u,y.size(),0,0,cv::INTER_LINEAR);
-	cv::resize(v,adj_v,y.size(),0,0,cv::INTER_LINEAR);
-	cv::Mat chan[3]={y,adj_u,adj_v};
+	cv::resize(v_u[frame_ptr],adj_u,s_y,0,0,cv::INTER_LINEAR);
+	cv::resize(v_v[frame_ptr],adj_v,s_y,0,0,cv::INTER_LINEAR);
+	cv::Mat chan[3]={v_y[frame_ptr],adj_u,adj_v};
 	cv::merge(chan,3,yuv);	
 	cv::cvtColor(yuv,bgr,cv::COLOR_YUV2BGR);
 	return bgr;
 }
 
-cv::Mat& y4m::get_y(){ return y; }
-cv::Mat& y4m::get_u(){ return u; }
-cv::Mat& y4m::get_v(){ return v; }
+uint y4m::get_width(){ return width; }
+uint y4m::get_height(){ return height; }
+uint8_t y4m::get_color_space(){ return color_space; }
+uint8_t y4m::get_interlace(){ return interlacing; }
+float y4m::get_fps(){ return fps[0]/fps[1]; }
+uint* y4m::get_framerate(){ return fps; }
+uint* y4m::get_aspect(){ return pix_asp; }
+uint y4m::get_num_frames(){ return v_y.size(); }
+cv::Mat& y4m::get_y(){ return v_y[frame_ptr]; }
+cv::Mat& y4m::get_u(){ return v_u[frame_ptr]; }
+cv::Mat& y4m::get_v(){ return v_v[frame_ptr]; }
 
 #include "opencv2/highgui/highgui.hpp" //FIXME temporary
 int main(int argc,char **argv){
-	y4m a;
-	std::string f(argv[1]);
-	a.load(f);
-	a.print_header();
+	if(argc!=4) return 1;
+	y4m in,out;
+	std::string f(argv[1]),ff(argv[2]),fff(argv[3]);
+	in.load(f);
+	in.print_header();
 
-	while(a.next_frame()){
-		cv::imshow("BGR",a.get_bgr());
-		cv::imshow("Y",a.get_y());
-		cv::imshow("U",a.get_u());
-		cv::imshow("V",a.get_v());
-		cv::waitKey(0);
-	}
+	do{
+		cv::imshow("BGR",in.get_bgr());
+		cv::imshow("Y",in.get_y());
+		cv::imshow("U",in.get_u());
+		cv::imshow("V",in.get_v());
+		cv::waitKey(1);
+	} while(in.next_frame());
+
+	in.seek(0);
+	out.init(in.get_width(),in.get_height(),in.get_color_space());	
+	//out.set_framerate((int) in.get_fps(),1);
+	out.set_framerate(in.get_framerate()[0],in.get_framerate()[1]);
+	out.set_aspect(in.get_aspect()[0],in.get_aspect()[1]);
+	out.set_interlace(in.get_interlace());
+
+	do{
+		out.push_frame(in.get_y(),in.get_u(),in.get_v());
+		cv::imshow("BGR",in.get_bgr());
+		cv::imshow("Y",in.get_y());
+		cv::imshow("U",in.get_u());
+		cv::imshow("V",in.get_v());
+		cv::waitKey(1);
+	} while(in.next_frame());
+	in.save(ff);
+	out.save(fff);
+	
 }
--- labwork3/src/lib/y4m/y4m.h
+++ labwork3/src/lib/y4m/y4m.h
@@ -1,11 +1,12 @@
-#include<fstream>
-#include<iostream>
-#include<string>
+#include <fstream>
+#include <iostream>
+#include <string>
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc.hpp"
 
 class y4m{
 	public:
+		//Constant values
 		static const uint IL_PROGRESSIVE=0; //IL = Interlacing
 		static const uint IL_TOP_FIELD_FIRST=1;
 		static const uint IL_BOT_FIELD_FIRST=2;
@@ -18,20 +19,40 @@ class y4m{
 		static const uint CS420JPEG=3;
 		static const uint CS420PALDV=4;
 
+		//IO
 		void print_header();
 		bool load(std::string &y4mfile);
+		void save(std::string &y4mfile);
 
+		//Creation
+		void init(uint width,uint height,uint8_t color_space);
+		void set_framerate(uint d,uint n);	
+		void set_aspect(uint d,uint n);	
+		void set_interlace(uint8_t intr);	
+		bool push_frame(cv::Mat &y,cv::Mat &u,cv::Mat &v);
+
+		//Reading 
+		uint8_t get_interlace();	
+		float get_fps();
+		uint* get_framerate();
+		uint* get_aspect();
+		uint get_width();
+		uint get_height();
+		uint8_t get_color_space();
 		bool next_frame();
-
+		bool seek(uint frame_number);
+		uint get_num_frames();
 		cv::Mat get_bgr();
 		cv::Mat& get_y();
 		cv::Mat& get_u();
 		cv::Mat& get_v();
 
 	private:
+		//Magic numbers
 		const std::string magic{"YUV4MPEG2 "};
 		const std::string frame_start{"FRAME"};
 
+		//IO
 		std::fstream inf,outf;
 
 		//Parameters
@@ -41,7 +62,16 @@ class y4m{
 		uint pix_asp[2];
 		uint8_t color_space;	
 		
-		//Mats	
-		cv::Mat y,u,v;
+		//Frames
+		std::vector<cv::Mat> v_y,v_u,v_v;
+		uint frame_ptr;
+		cv::Size s_y,s_u,s_v;
+		
+		//Support functions
+		void build_structure();
+		bool load_header();
+		void save_header();
+		bool fetch();
+		void store_all();
 
 };
