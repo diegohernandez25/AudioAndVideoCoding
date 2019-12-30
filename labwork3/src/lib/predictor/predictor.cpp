cd19c90952d2d913d2d2c7e7df7b1608b917a3c8
Margarida Silva <margaridaocs@ua.pt>
2019-12-30 21:13:54 +0000

    WIP: implement 7 jpeg prediction modes

--- labwork3/src/lib/predictor/predictor.cpp
+++ labwork3/src/lib/predictor/predictor.cpp
@@ -17,55 +17,57 @@ void predictor::newFrame(cv::Mat* mat) {
 short predictor::predict(ushort x, ushort y, uint8_t mode) {
     short ret = 0;
 
-    /*switch(mode) {
+    switch(mode) {
         case 1:
-            if (ptr == 0)
+            if (x == 0)
                 return ret; 
             // A
-            ret = short(mat[ptr - 1]);
+            ret = short(origMat->at<uchar>(y,x-1));
             break;
         case 2:
-            if (ptr < width)
+            if (y == 0)
                 return ret; 
             // B
-            ret = short(mat[ptr - width]);
+            ret = short(origMat->at<uchar>(y-1,x));
             break;
         case 3:
-            if (ptr < (width - 1))
+            if (x == 0 || y == 0)
                 return ret; 
             // C
-            ret = short(mat[ptr - width - 1]);
+            ret = short(origMat->at<uchar>(y-1,x-1));
             break;
         case 4:
-            if (ptr < (width - 1))
+            if (x == 0 || y == 0)
                 return ret; 
             // A+B-C
-            ret = short(mat[ptr - 1]) + short(mat[ptr - width]) - short(mat[ptr - width - 1]);
+            ret = short(origMat->at<uchar>(y,x-1)) + short(origMat->at<uchar>(y-1,x)) - short(origMat->at<uchar>(y-1,x-1));
             break;
         case 5:
-            if (ptr < (width - 1))
+            if (x == 0 || y == 0)
                 return ret; 
             // A+(B-C)/2
-            ret = short(mat[ptr - 1]) + (short(mat[ptr - width]) - short(mat[ptr - width - 1]))/2;
+            ret = short(origMat->at<uchar>(y,x-1)) + (short(origMat->at<uchar>(y-1,x)) - short(origMat->at<uchar>(y-1,x-1)))/2;
             break;
         case 6:
-            if (ptr < (width - 1))
+            if (x == 0 || y == 0)
                 return ret; 
             // B+(A-C)/2
-            ret = short(mat[ptr - width]) + (short(mat[ptr - 1]) - short(mat[ptr - width - 1]))/2;
+            ret = short(origMat->at<uchar>(y-1,x)) + (short(origMat->at<uchar>(y,x-1)) - short(origMat->at<uchar>(y-1,x-1)))/2;
             break;
         case 7:
-            if (ptr < (width - 1))
+            if (x == 0 || y == 0)
                 return ret; 
             // (A+B)/2
-            ret = (short(mat[ptr - 1]) + short(mat[ptr - width]))/2;
+            ret = (short(origMat->at<uchar>(y,x-1)) + short(origMat->at<uchar>(y-1,x)))/2;
             break;
-    }*/
+    }
+    if (ret < 0)
+        ret = 0;
     return ret;
 }
 
 short predictor::calcResidual(ushort x, ushort y, uint8_t mode) {
-    return short(origMat->at<uchar>(x, y)) - predict(x, y, mode);
+    return short(origMat->at<uchar>(y,x)) - predict(x, y, mode);
 }
 
 void predictor::calcBlockResiduals(ushort x, ushort y, uint8_t mode, cv::Mat* resMat) {
@@ -77,7 +79,7 @@ void predictor::calcBlockResiduals(ushort x, ushort y, uint8_t mode, cv::Mat* re
 }
 
 void predictor::reconstruct(ushort x, ushort y, uint8_t mode, short residual) {
-    origMat->at<uchar>(x,y) = predict(x, y, mode) + residual;
+    origMat->at<uchar>(y,x) = predict(x, y, mode) + residual;
 }
 
 void predictor::reconstructBlock(ushort x, ushort y, uint8_t mode, cv::Mat* resMat) {
