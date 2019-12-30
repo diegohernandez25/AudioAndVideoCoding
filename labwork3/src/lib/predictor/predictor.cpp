fe0277e3ec5be6743bb043b595d02802b0931d2f
Margarida Silva <margaridaocs@ua.pt>
2019-12-30 18:54:40 +0000

    WIP: 7 JPEG predictor modes (uchar*)

--- labwork3/src/lib/predictor/predictor.cpp
+++ labwork3/src/lib/predictor/predictor.cpp
@@ -16,22 +16,52 @@ void predictor::setMat(uchar* mat) {
 }
 
 short predictor::predict() {
-    // TODO start with 0?
     short ret = 0;
 
     switch(mode) {
         case 1:
             if (ptr == 0)
                 return ret; 
+            // A
             ret = short(mat[ptr - 1]);
             break;
         case 2:
             if (ptr < width)
                 return ret; 
+            // B
             ret = short(mat[ptr - width]);
             break;
+        case 3:
+            if (ptr < (width - 1))
+                return ret; 
+            // C
+            ret = short(mat[ptr - width - 1]);
+            break;
+        case 4:
+            if (ptr < (width - 1))
+                return ret; 
+            // A+B-C
+            ret = short(mat[ptr - 1]) + short(mat[ptr - width]) - short(mat[ptr - width - 1]);
+            break;
+        case 5:
+            if (ptr < (width - 1))
+                return ret; 
+            // A+(B-C)/2
+            ret = short(mat[ptr - 1]) + (short(mat[ptr - width]) - short(mat[ptr - width - 1]))/2;
+            break;
+        case 6:
+            if (ptr < (width - 1))
+                return ret; 
+            // B+(A-C)/2
+            ret = short(mat[ptr - width]) + (short(mat[ptr - 1]) - short(mat[ptr - width - 1]))/2;
+            break;
+        case 7:
+            if (ptr < (width - 1))
+                return ret; 
+            // (A+B)/2
+            ret = (short(mat[ptr - 1]) + short(mat[ptr - width]))/2;
+            break;
     }
-
     return ret;
 }
 
