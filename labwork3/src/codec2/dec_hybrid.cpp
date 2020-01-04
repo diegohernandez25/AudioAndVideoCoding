ec0581deb6b4fe9b4ed58cc1f2a2056f6c32085d
André Pinho <andre.s.pinho@ua.pt>
2020-01-04 03:41:22 +0000

    Codec2 implementation

--- labwork3/src/codec2/dec_hybrid.cpp
+++ labwork3/src/codec2/dec_hybrid.cpp
@@ -1,10 +1,206 @@
-#include <iostream>
-#include "../lib/args/args.h"
+#include "dec_hybrid.h"
 
-using namespace std;
+dec_hybrid::dec_hybrid(args& cfg):
+    cfg(cfg),
+    bss(cfg.fileIn.c_str(),std::ios::binary|std::ios::in),
+    bs(bss,true),
+    gb_y(golomb_initial_m,golomb_blk_size,golomb_calc_interval,bs),
+    gb_u(golomb_initial_m,golomb_blk_size,golomb_calc_interval,bs),
+    gb_v(golomb_initial_m,golomb_blk_size,golomb_calc_interval,bs){}
+
+
+int dec_hybrid::decode(){
+
+	//Check magic
+	char is_magic[strlen(magic)];
+	bs.readNChars(is_magic,sizeof(is_magic));
+	if(strncmp(magic,is_magic,strlen(magic))!=0) return -1;
+	
+	//Read Width/Height (Y4M)
+	uint width=bs.readNBits(sizeof(uint)*8);
+	uint height=bs.readNBits(sizeof(uint)*8);
+
+	//Read Color Space/Interlace Mode (Y4M)
+	uint8_t color_space=bs.readNBits(sizeof(uint8_t)*8);
+	uint8_t interlace=bs.readNBits(sizeof(uint8_t)*8);
+
+	//Read Frame Rate (Y4M)
+	uint framerate[2];
+	framerate[0]=bs.readNBits(sizeof(uint)*8);
+	framerate[1]=bs.readNBits(sizeof(uint)*8);
+
+	//Read Aspect Ratio (Y4M)
+	uint aspect[2];
+	aspect[0]=bs.readNBits(sizeof(uint)*8);
+	aspect[1]=bs.readNBits(sizeof(uint)*8);
+
+	//Read Number of Frames
+	uint num_frames=bs.readNBits(sizeof(uint)*8);
+
+	//Read Forced Prediction
+	pred_mode=bs.readNBits(sizeof(uint8_t)*8);
+
+	//Read block size
+	predBlockSize=bs.readNBits(sizeof(uint)*8);
+
+	//Read macroblock size
+	macroSize=bs.readNBits(sizeof(uint)*8);
+
+	//Read search depth
+	uint compensator_depth=bs.readNBits(sizeof(uint16_t)*8);
+
+	//Create new video
+	out.init(width,height,color_space,predBlockSize);
+	out.set_framerate(framerate[0],framerate[1]);
+	out.set_aspect(aspect[0],aspect[1]);
+	out.set_interlace(interlace);
+	out.print_header();
+
+	cp=compensator(predBlockSize*macroSize,0,0);
+    hist_y=boost::circular_buffer<cv::Mat>(compensator_depth);
+    hist_u=boost::circular_buffer<cv::Mat>(compensator_depth);
+    hist_v=boost::circular_buffer<cv::Mat>(compensator_depth);
+
+	pd_y=predictor(out.get_bsize_y().width,out.get_bsize_y().height);
+	pd_u=predictor(out.get_bsize_uv().width,out.get_bsize_uv().height);
+	pd_v=predictor(out.get_bsize_uv().width,out.get_bsize_uv().height);
+
+	res_y=cv::Mat(out.get_bsize_y(),CV_16S);
+    res_u=cv::Mat(out.get_bsize_uv(),CV_16S);
+    res_v=cv::Mat(out.get_bsize_uv(),CV_16S);
+
+	//Get Data
+	for(uint f=0;f<num_frames;f++){
+		cv::Mat y(out.get_pady_size(),CV_8U);
+		cv::Mat u(out.get_paduv_size(),CV_8U);
+		cv::Mat v(out.get_paduv_size(),CV_8U);
+
+		pd_y.newFrame(&y);
+		pd_u.newFrame(&u);
+		pd_v.newFrame(&v);
+
+		bool iframe=bs.readBit();
+
+		if(iframe)
+			i_frame();
+		else{
+			p_frame(y,u,v);
+		}
+
+		hist_y.push_back(y);
+		hist_u.push_back(u);
+		hist_v.push_back(v);
+
+		out.push_frame(y,u,v);
+	
+	}
+	out.save(cfg.fileOut);
+	return 0;
+}
+
+void dec_hybrid::i_frame(){
+	std::cout<<"I"<<std::flush;
+	for(uint by=0;by*predBlockSize<(uint)out.get_y_size().height;by++){
+		for(uint bx=0;bx*predBlockSize<(uint)out.get_y_size().width;bx++){
+			read_block(bx,by);
+		}
+	}
+}
+
+void dec_hybrid::p_frame(cv::Mat& y,cv::Mat& u,cv::Mat& v){
+    std::cout<<"P"<<std::flush;
+    for(uint mby=0;mby*predBlockSize*macroSize<(uint)y.rows;mby++){
+        for(uint mbx=0;mbx*predBlockSize*macroSize<(uint)y.cols;mbx++){
+			bool iblock=bs.readBit();
+
+            if(iblock){ //Decode with intra (I-Block)
+                //Iterate through blocks of macroblock
+                for(uint by=mby*macroSize;by<(mby+1)*macroSize&&by*predBlockSize<(uint)y.rows;by++){
+                    for(uint bx=mbx*macroSize;bx<(mbx+1)*macroSize&&bx*predBlockSize<(uint)y.cols;bx++){
+
+                        read_block(bx,by);
+                    }
+                }
+            }
+            else{ //Decode with inter (P-Block)
+                read_macroblock(mbx,mby,y,u,v);
+            }
+        }
+    }
+}
+
+void dec_hybrid::read_macroblock(uint mbx,uint mby,cv::Mat& y,cv::Mat& u,cv::Mat& v){
+	uint framenr=bs.readNBits(sizeof(ushort)*8); //Frame Nr
+	uint vecx=bs.readNBits(sizeof(uint)*8); //VecX
+	uint vecy=bs.readNBits(sizeof(uint)*8); //VecY
+	cv::Vec3w mvec(framenr,vecx,vecy);
+
+    uint mbw_y=out.get_bsize_y().width*macroSize;
+    uint mbh_y=out.get_bsize_y().height*macroSize;
+    uint mbw_uv=out.get_bsize_uv().width*macroSize;
+    uint mbh_uv=out.get_bsize_uv().height*macroSize;
+
+    uint macroy_x=mbw_y;
+    uint macroy_y=mbh_y;
+    uint macrouv_x=mbw_uv;
+    uint macrouv_y=mbh_uv;
+    if(mbx*mbw_y+mbw_y>(uint)y.cols){
+        std::cout<<"hey"<<std::endl;
+        macroy_x=y.cols-mbx*mbw_y;
+        macrouv_x=u.cols-mbx*mbw_uv;
+    }
+    if(mby*mbh_y+mbh_y>(uint)y.rows){
+        std::cout<<"ho"<<std::endl;
+        macroy_y=y.rows-mby*mbh_y;
+        macrouv_y=u.rows-mby*mbh_uv;
+    }
+
+	res_macro_y=cv::Mat(macroy_y,macroy_x,CV_16S);	
+	res_macro_u=cv::Mat(macrouv_y,macrouv_x,CV_16S);	
+	res_macro_v=cv::Mat(macrouv_y,macrouv_x,CV_16S);	
+
+    gb_y.read_mat(res_macro_y,true);
+    gb_u.read_mat(res_macro_u,true);
+    gb_v.read_mat(res_macro_v,true);
+
+	cv::Mat blky=y(cv::Rect_<uint>(mbx*mbw_y,mby*mbh_y,macroy_x,macroy_y));
+	cv::Mat blku=u(cv::Rect_<uint>(mbx*mbw_uv,mby*mbh_uv,macrouv_x,macrouv_y));
+	cv::Mat blkv=v(cv::Rect_<uint>(mbx*mbw_uv,mby*mbh_uv,macrouv_x,macrouv_y));
+
+    cp.restore_block(blky,res_macro_y,hist_y,mvec);
+
+    //readjust vectors sizes
+    mvec[1]/=mbw_y/mbw_uv;
+    mvec[2]/=mbh_y/mbh_uv;
+
+	cp.restore_block(blku,res_macro_u,hist_u,mvec);
+	cp.restore_block(blkv,res_macro_v,hist_v,mvec);
+}
+
+void dec_hybrid::read_block(uint bx,uint by){
+	uint bw_y=out.get_bsize_y().width;
+	uint bh_y=out.get_bsize_y().height;
+	uint bw_uv=out.get_bsize_uv().width;
+	uint bh_uv=out.get_bsize_uv().height;
+
+	uint8_t best_pred;
+	if(pred_mode==9){ //NOT forced mode
+		best_pred=bs.readNBits(4);
+	}
+	else{
+		best_pred=pred_mode;
+	}
+	gb_y.read_mat(res_y,true);
+	gb_u.read_mat(res_u,true);
+	gb_v.read_mat(res_v,true);
+
+	pd_y.reconstructBlock(bx*bw_y,by*bh_y,best_pred,&res_y);
+	pd_u.reconstructBlock(bx*bw_uv,by*bh_uv,best_pred,&res_u);
+	pd_v.reconstructBlock(bx*bw_uv,by*bh_uv,best_pred,&res_v);
+}
 
 int main(int argc,char** argv){
 	args cfg = args(argc, argv, 0);
-
-	// TODO decode hybrid
+    dec_hybrid d(cfg);
+	return d.decode();
 }
--- labwork3/src/codec2/enc_hybrid.cpp
+++ labwork3/src/codec2/enc_hybrid.cpp
@@ -1,29 +1,197 @@
-#include <iostream>
-#include "../lib/args/args.h"
-#include "../lib/y4m/y4m.h"
+#include "enc_hybrid.h"
 
-using namespace std;
+//JPEG pred (0-7 Normal JPEG Preditors, 8 JPEG-LS, 9 Auto prediction)
+
+enc_hybrid::enc_hybrid(args& cfg):
+	cfg(cfg),
+	bss(cfg.fileOut.c_str(),std::ios::trunc|std::ios::binary|std::ios::out),
+	bs(bss,true),
+	gb_y(golomb_initial_m,golomb_blk_size,golomb_calc_interval,bs),
+	gb_u(golomb_initial_m,golomb_blk_size,golomb_calc_interval,bs),
+	gb_v(golomb_initial_m,golomb_blk_size,golomb_calc_interval,bs){}
+
+void enc_hybrid::encode(){
+	in.load(cfg.fileIn,cfg.blockSize);
+	in.print_header();
+
+	pd_y=predictor(in.get_bsize_y().width,in.get_bsize_y().height);
+	pd_u=predictor(in.get_bsize_uv().width,in.get_bsize_uv().height);
+	pd_v=predictor(in.get_bsize_uv().width,in.get_bsize_uv().height);
+
+	cp=compensator(cfg.macroSize*cfg.blockSize,cfg.searchArea,compensator_lazy_score); //TODO add to ARGS search_depth and lazy_score
+	hist_y=boost::circular_buffer<cv::Mat>(compensator_depth);
+	hist_u=boost::circular_buffer<cv::Mat>(compensator_depth);
+	hist_v=boost::circular_buffer<cv::Mat>(compensator_depth);
+	
+	//Write magic
+    bs.writeNChars((char*) magic,strlen(magic));
+
+	//Write Width/Height (Y4M)
+	bs.writeNBits(in.get_width(),sizeof(uint)*8);
+	bs.writeNBits(in.get_height(),sizeof(uint)*8);
+
+	//Write Color Space/Interlace Mode (Y4M)
+	bs.writeNBits(in.get_color_space(),sizeof(uint8_t)*8);
+	bs.writeNBits(in.get_interlace(),sizeof(uint8_t)*8);
+
+	//Write Frame Rate (Y4M)
+	bs.writeNBits(in.get_framerate()[0],sizeof(uint)*8);
+	bs.writeNBits(in.get_framerate()[1],sizeof(uint)*8);
+
+	//Write Aspect Ratio (Y4M)
+	bs.writeNBits(in.get_aspect()[0],sizeof(uint)*8);
+	bs.writeNBits(in.get_aspect()[1],sizeof(uint)*8);
+
+	//Write Number of Frames
+	bs.writeNBits(in.get_num_frames(),sizeof(uint)*8);
+
+	//Write Forced Prediction
+	bs.writeNBits(cfg.jpegPredictor,sizeof(uint8_t)*8);
+
+	//Write block size
+	bs.writeNBits(cfg.blockSize,sizeof(uint)*8);
+
+	//Write macroblock size
+	bs.writeNBits(cfg.macroSize,sizeof(uint)*8);
+
+	//Write search depth
+	bs.writeNBits(compensator_depth,sizeof(uint16_t)*8);
+
+	res_y=cv::Mat(in.get_bsize_y(),CV_16S);
+	res_u=cv::Mat(in.get_bsize_uv(),CV_16S);
+	res_v=cv::Mat(in.get_bsize_uv(),CV_16S);
+
+	bool first_frame=true;
+	//Add Data
+	do{
+		cv::Mat y=in.get_y(); 
+		cv::Mat u=in.get_u();
+		cv::Mat v=in.get_v();
+
+		pd_y.newFrame(&y);
+		pd_u.newFrame(&u);
+		pd_v.newFrame(&v);
+
+		if(first_frame){
+			i_frame();
+			first_frame=false;
+		}
+		else{
+			p_frame(y,u,v);
+		}
+
+		hist_y.push_back(y);
+		hist_u.push_back(u);
+		hist_v.push_back(v);
+	}while(in.next_frame());	
+}
+
+void enc_hybrid::i_frame(){
+	std::cout<<"I"<<std::flush;
+	bs.writeBit(1); //Means I-Frame
+	for(uint by=0;by*cfg.blockSize<(uint)in.get_y_size().height;by++){
+		for(uint bx=0;bx*cfg.blockSize<(uint)in.get_y_size().width;bx++){
+			write_block(bx,by);
+		}
+	}
+}
+
+void enc_hybrid::p_frame(cv::Mat& y,cv::Mat& u,cv::Mat& v){
+	std::cout<<"P"<<std::flush;
+	bs.writeBit(0); //Means P-Frame
+
+	cv::Mat matches=cp.find_matches(y,hist_y);
+	for(uint mby=0;mby*cfg.blockSize*cfg.macroSize<(uint)y.rows;mby++){
+		for(uint mbx=0;mbx*cfg.blockSize*cfg.macroSize<(uint)y.cols;mbx++){
+			if(matches.at<cv::Vec4w>(mby,mbx)[0]>macroblock_threshold){ //Code with intra (I-Block)
+				bs.writeBit(1); //Means I-Block
+				//Iterate through blocks of macroblock
+				for(uint by=mby*cfg.macroSize;by<(mby+1)*cfg.macroSize&&by*cfg.blockSize<(uint)y.rows;by++){ 
+					for(uint bx=mbx*cfg.macroSize;bx<(mbx+1)*cfg.macroSize&&bx*cfg.blockSize<(uint)y.cols;bx++){ 
+						write_block(bx,by);
+					}
+				}
+			}
+			else{ //Code with inter (P-Block)
+				bs.writeBit(0); //Means P-Block
+				write_macroblock(mbx,mby,matches.at<cv::Vec4w>(mby,mbx),y,u,v);
+				
+			}
+		}
+	}
+}
+
+void enc_hybrid::write_macroblock(uint mbx,uint mby,cv::Vec4w mvec,cv::Mat& y,cv::Mat& u,cv::Mat& v){
+	uint mbw_y=in.get_bsize_y().width*cfg.macroSize;
+	uint mbh_y=in.get_bsize_y().height*cfg.macroSize;
+	uint mbw_uv=in.get_bsize_uv().width*cfg.macroSize;
+	uint mbh_uv=in.get_bsize_uv().height*cfg.macroSize;
+
+	uint macroy_x=mbw_y;
+	uint macroy_y=mbh_y;
+	uint macrouv_x=mbw_uv;
+	uint macrouv_y=mbh_uv;
+	if(mbx*mbw_y+mbw_y>(uint)y.cols){
+		std::cout<<"hey"<<std::endl;
+		macroy_x=y.cols-mbx*mbw_y;
+		macrouv_x=u.cols-mbx*mbw_uv;
+	}
+	if(mby*mbh_y+mbh_y>(uint)y.rows){
+		std::cout<<"ho"<<std::endl;
+		macroy_y=y.rows-mby*mbh_y;
+		macrouv_y=u.rows-mby*mbh_uv;
+	}
+	y(cv::Rect_<uint>(mbx*mbw_y,mby*mbh_y,macroy_x,macroy_y)).convertTo(res_macro_y,CV_16S);
+	u(cv::Rect_<uint>(mbx*mbw_uv,mby*mbh_uv,macrouv_x,macrouv_y)).convertTo(res_macro_u,CV_16S);
+	v(cv::Rect_<uint>(mbx*mbw_uv,mby*mbh_uv,macrouv_x,macrouv_y)).convertTo(res_macro_v,CV_16S);
+
+	bs.writeNBits(mvec[1],sizeof(ushort)*8); //Frame Nr
+	bs.writeNBits(mvec[2],sizeof(uint)*8); //VecX
+	bs.writeNBits(mvec[3],sizeof(uint)*8); //VecY
+
+	cp.apply_block_residual(res_macro_y,hist_y,mvec);
+
+	//readjust vectors sizes
+	mvec[2]/=mbw_y/mbw_uv;
+	mvec[3]/=mbh_y/mbh_uv;
+
+	cp.apply_block_residual(res_macro_u,hist_u,mvec);
+	cp.apply_block_residual(res_macro_v,hist_v,mvec);
+
+	gb_y.write_mat(res_macro_y,true);
+	gb_u.write_mat(res_macro_u,true);
+	gb_v.write_mat(res_macro_v,true);
+		
+}
+
+void enc_hybrid::write_block(uint bx,uint by){
+	uint bw_y=in.get_bsize_y().width;
+	uint bh_y=in.get_bsize_y().height;
+	uint bw_uv=in.get_bsize_uv().width;
+	uint bh_uv=in.get_bsize_uv().height;
+	
+	uint8_t best_pred;
+	if(cfg.jpegPredictor==9){ //NOT forced mode
+		best_pred=std::get<0>(pd_y.calcBestResiduals(bx*bw_y,by*bh_y,&res_y));//FIXME do something with score
+		//Maybe return tuple and let the other function decide
+		bs.writeNBits(best_pred,4);
+	}
+	else{
+		best_pred=cfg.jpegPredictor;
+		pd_y.calcBlockResiduals(bx*bw_y,by*bh_y,best_pred,&res_y);
+	}
+
+	pd_u.calcBlockResiduals(bx*bw_uv,by*bh_uv,best_pred,&res_u);
+	pd_v.calcBlockResiduals(bx*bw_uv,by*bh_uv,best_pred,&res_v);
+
+	gb_y.write_mat(res_y,true);
+	gb_u.write_mat(res_u,true);
+	gb_v.write_mat(res_v,true);
+
+}
 
 int main(int argc,char** argv){
 	args cfg = args(argc, argv, 2);
-	y4m cap;
-	cv::Mat frame;
-
-	if(!cap.load(cfg.fileIn,0)){
-		std::cout << "ERROR: couldn't open video file" << std::endl;
-		return -1;
-	}
-
-	while (true) {
-		// Get next frame
-		frame=cap.get_bgr();
-
-		if (frame.empty())
-			break;
-
-		// TODO encode hybrid
-
-		if(!cap.next_frame())
-			break;
-	}
+	enc_hybrid e(cfg);
+	e.encode();
 }
