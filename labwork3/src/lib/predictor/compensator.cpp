1e7c26bccb3175961a4a52b3daaa03d11fe5033f
André Pinho <andre.s.pinho@ua.pt>
2020-01-06 17:29:54 +0000

    Removed compensator print

--- labwork3/src/lib/predictor/compensator.cpp
+++ labwork3/src/lib/predictor/compensator.cpp
@@ -33,7 +33,6 @@ cv::Mat compensator::find_matches(cv::Mat& curr,boost::circular_buffer<cv::Mat>&
 		for(uint bx=0;bx<(uint)curr.cols;bx+=macroblock_size){
 			uint macrox=macroblock_size;
 			uint macroy=macroblock_size;
-			bool print=false;
 			if(bx+macroblock_size>(uint)curr.cols){//last macro blocks blocks might not be complete
 				macrox=curr.cols-bx;
 			}
@@ -60,7 +59,6 @@ cv::Mat compensator::find_matches(cv::Mat& curr,boost::circular_buffer<cv::Mat>&
 							entry[1]=fr; //Frame Nr
 							entry[2]=cx; //MVecX
 							entry[3]=cy; //MVecY
-							if(print) std::cout<<cv::Rect_<uint>(cx,cy,macrox,macroy)<<frame.size()<<std::endl;
 
 							if(curr_score<=lazy_score){ //jump to next block if satistied
 								goto block_iter;
@@ -69,7 +67,6 @@ cv::Mat compensator::find_matches(cv::Mat& curr,boost::circular_buffer<cv::Mat>&
 					}
 				}
 			}
-			//if(print) std::cout<<"FINAL"<<cv::Rect_<uint>(scores.at<cv::Vec4w>(by/macroblock_size,bx/macroblock_size)[2],scores.at<cv::Vec4w>(by/macroblock_size,bx/macroblock_size)[3],macrox,macroy)<<std::endl;
 block_iter: ;
 		}
 	}
